b7f00dd99b2fa317039a9356e058129f12acdc51
DomBro <460716809@qq.com>
2018-11-29 07:48:41 +0800

    MODFUNC
    MODFUNC:
    1. 修改crc32函数。//crc32_util.go

--- utils/crc32_util.go
+++ utils/crc32_util.go
@@ -2,8 +2,18 @@ package utils
 
 import (
 	"akita/db"
+	"hash/crc32"
 )
 
 func GetCrc32(header *db.DataHeader, key []byte, value []byte) uint32 {
-	return 0
+	//使用 header + key + value 的 byte 数组 形成 crc32
+	ksBuf, _ := IntToByteSlice(header.Ks)
+	vsBuf, _ := IntToByteSlice(header.Vs)
+	flagBuf, _ := IntToByteSlice(header.Flag)
+	buf := append(ksBuf, vsBuf...)
+	buf = append(buf, flagBuf...)
+	buf = append(buf, key...)
+	buf = append(buf, value...)
+	crcValue := crc32.ChecksumIEEE(buf)
+	return crcValue
 }

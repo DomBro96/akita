841bac1b5cefc81fbf517781b0202d8b502496b0
dongbo <dongbo@qingsongchou.com>
2018-11-30 19:14:54 +0800

    MODFUNC DELMOUD ADDSTRUCT
    MODFUNC:
    1. 修改 core_map 的 set, get, del  的接收者。
    
    DELMOUD:
    1. 删除 data_curd 模块， 使用 Db 莫跨替代。 // database.go
    
    ADDSTRUCT:
    1. 增加 connection 结构体类型，
    作为上层获取连接的接口。//database.go(Connection)

--- db/coremap/core_map.go
+++ db/coremap/core_map.go
@@ -1,5 +1,7 @@
 package coremap
 
+import "akita/db"
+
 type coreMap struct {
 	Map       map[string]int64
 	CurOffset int64
@@ -7,9 +9,21 @@ type coreMap struct {
 
 var MapInstance *coreMap
 
-func SingleCoreMap() *coreMap {
+func SingletonCoreMap() *coreMap {
 	if MapInstance == nil {
 		MapInstance = &coreMap{}
 	}
 	return MapInstance
+}
+
+func (cm *coreMap) set(record *db.AkitaRecord) (bool, error) {
+	return false, nil
+}
+
+func (cm *coreMap) get(key string) (int64, error)  {
+	return 0, nil
+}
+
+func (cm *coreMap) del(key string) (bool, error) {
+	return false, nil
 }
---
+++ db/database.go
@@ -0,0 +1,31 @@
+package db
+
+type AkitaDb struct {
+
+}
+
+
+
+type Connection struct {
+
+}
+
+
+
+func (db *AkitaDb) Reload() (bool, error) {							   		//数据重新载入
+	return false, nil
+}
+
+func (conn *Connection) Insert(key string, fileName string) (bool, error) {	//插入数据
+	return false, nil
+}
+
+func (conn *Connection) Lookup(key string) ([]byte, error) {				//查找数据
+	return nil, nil
+}
+
+func (conn *Connection) Delete(key string) (bool, error)  {					//删除数据
+	return false,  nil
+}
+
+

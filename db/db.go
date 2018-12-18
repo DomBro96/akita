5e8c940ff4f7920ee59f0cb63f2c2d03552dd842
dombro <460716809@qq.com>
2018-12-18 23:17:10 +0800

    MODFUNC
    MODFUNC:
    1. 编辑 OpenDB 函数， 当初始化 db 对象失败时，为防止空指针退出程序。
    2. 完善 Reload 函数， 作为重启时的重要函数。

--- db/db.go
+++ db/db.go
@@ -16,39 +16,81 @@ type DB struct {
 	iTable   *indexTable // 数据索引
 }
 
-
+// 在 OpenDB 函数中出错，直接退出程序
 func OpenDB(path string) *DB {
 	dir := filepath.Dir(path)
 	ok, err := common.FileIsExit(dir)
 	if err != nil {
+		common.Error.Fatalf("Get data dir is exit error: %s\n", err)
 		return nil
 	}
 	if !ok {
 		err = os.Mkdir(dir, os.ModePerm)
 		if err != nil {
-			common.Error.Printf("Make data file folder error: %s\n", err)
+			common.Error.Fatalf("Make data file folder error: %s\n", err)
 			return nil
 		}
 	}
 	dbFile, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
 	if err != nil {
-		common.Error.Printf("Open data file error: %s\n", err)
+		common.Error.Fatalf("Open data file error: %s\n", err)
+		return nil
+	}
+	fs, err := common.GetFileSize(dbFile)
+	if err != nil {
+		common.Error.Fatalf("Get data file size error: %s\n", err)
 		return nil
 	}
 	db := &DB{
 		dataFile: dbFile,
-		size:     0,
+		size:     fs,
 		iTable:   newIndexTable(),
 	}
 	return db
 }
 
-func (db *DB) Reload() (bool, error) { // 数据重新载入
-	return false, nil
+// 数据重新载入， 重建索引的过程
+func (db *DB) Reload() error {
+	var offset int64 = 0
+	dataBuff, err := common.ReadFileToByte(db.dataFile, offset, db.size)
+	if err != nil {
+		return err
+	}
+	for offset != db.size {
+		ksBuf := dataBuff[offset:common.KsByteLength]
+		vsBuf := dataBuff[(offset + common.KsByteLength):(offset + common.KvsByteLength)]
+		flagBuf := dataBuff[(offset + common.KvsByteLength):(offset + common.KvsByteLength + common.FlagByteLength)]
+		ks, err := common.ByteSliceToInt32(ksBuf)
+		if err != nil {
+			return err
+		}
+		vs, err := common.ByteSliceToInt32(vsBuf)
+		if err != nil {
+			return err
+		}
+		flag, err := common.ByteSliceToInt32(flagBuf)
+		if err != nil {
+			return err
+		}
+		keyBuf := dataBuff[(offset + common.KvsByteLength + common.FlagByteLength):(offset + common.KvsByteLength + common.FlagByteLength + int64(ks))]
+		key := common.ByteSliceToString(keyBuf)
+	    rs := common.KvsByteLength + common.FlagByteLength + common.CrcByteLength + int(ks) + int(vs)
+		if flag == common.DeleteFlag {
+			db.iTable.remove(key)
+		}else {
+			ri := recordIndex{
+				offset: offset,
+				size: rs,
+			}
+			db.iTable.put(key, &ri)
+		}
+		offset += int64(rs)
+	}
+	return nil
 }
 
 // 向数据文件中写入一条记录
-func (db *DB) WriteRecord(record *dataRecord) (int64, error) { // 将记录写入
+func (db *DB) WriteRecord(record *dataRecord) (int64, error) {
 	ksBuf, err := common.Int32ToByteSlice(record.dateHeader.Ks)
 	if err != nil {
 		common.Error.Printf("Turn int32 to byte slice error: %s\n", err )
@@ -109,8 +151,4 @@ func (db *DB) ReadRecord(offset int64, length int64) ([]byte, error) {
 		return nil, common.ErrDataHasBeenModified
 	}
 	return valueBuf, nil
-}
-
-func (s *Server) Close() error { // 关闭连接, 使 Server 实现 io.Closer
-	return nil
 }

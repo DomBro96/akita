e7f23d89508c44835d6d37d0e4ff60a226609a28
阮胜 <ruansheng@qingsongchou.com>
2019-03-04 18:50:10 +0800

    web ui

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"akita/common"
 	"akita/db"
 	"fmt"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/takama/daemon"
+	"html/template"
+	"io"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,6 +19,7 @@ import (
 const (
 	name        = "akita"
 	description = "Akita: A Simple Key-Value Database"
+	webAddr     = "0.0.0.0:8989"
 )
 
 var dependencies = []string{"labstack.echo", "takama.daemon"}
@@ -52,7 +58,7 @@ func (service *Service) Manage() (string, error) {
 		go func() {
 			for {
 				db.Sev.DbSync()
-				time.Sleep(500 * time.Millisecond)	// do sync request every half second
+				time.Sleep(500 * time.Millisecond) // do sync request every half second
 			}
 		}()
 	}
@@ -70,6 +76,49 @@ func (service *Service) Manage() (string, error) {
 	}
 }
 
+// http://127.0.0.1:8989
+func (service *Service) WebUI(webAddr string) {
+	e := echo.New()
+	// static path
+	e.Static("/", "static")
+
+	// Logger middleware
+	e.Use(middleware.Logger())
+
+	// Recover middleware
+	e.Use(middleware.Recover())
+
+	// 注册模板
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+	e.Renderer = t
+
+	service.webUIRouter(e)
+
+	e.Logger.Fatal(e.Start(webAddr))
+}
+
+// router
+func (service *Service) webUIRouter(e *echo.Echo) {
+	e.GET("/", service.webUIIndex)
+}
+
+// webUI Index page
+func (service *Service) webUIIndex(c echo.Context) error {
+	data := make(map[string]interface{})
+
+	return c.Render(http.StatusOK, "index.html", data)
+}
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	srv, err := daemon.New(name, description, dependencies...)
 	if err != nil {
@@ -80,5 +129,9 @@ func main() {
 	if err != nil {
 		common.Error.Fatalf(status, err)
 	}
+
+	// start webUI
+	go service.WebUI(webAddr)
+
 	fmt.Println(status)
 }

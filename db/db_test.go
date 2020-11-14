0a7fa7f12580ca6a9d5c8b563c0bfe03998cdd75
DomBro96 <460716809@qq.com>
2020-11-14 23:23:55 +0800

    add unit test.

--- db/db_test.go
+++ db/db_test.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"bytes"
+	"encoding/binary"
 	"hash/crc32"
 	"testing"
 )
@@ -110,6 +112,108 @@ func Test_UpdateTable(t *testing.T) {
 
 	err := d.UpdateTable(0, d.size)
 	if err != nil {
-		t.Logf("")
+		t.Logf("test update db table error: %s. \n", err)
+		return
 	}
+}
+
+func Test_GetDataByOffset(t *testing.T) {
+	t.Log("test get data by offset.")
+	d := OpenDB("/usr/local/akdata/akita.dat")
+
+	t.Logf("test get data by offset =====> size: %d. \n", d.size)
+
+	data, err := d.GetDataByOffset(0)
+	if err != nil {
+		t.Errorf("test get data by offset error: %s. \n", err)
+		return
+	}
+
+	t.Logf("test get data by offset =====> data lenth :%d. \n", len(data))
+}
+
+func Test_WriteSyncData(t *testing.T) {
+	t.Log("test write sync data.")
+
+	d := OpenDB("/usr/local/akdata/akita.dat")
+	key := "test2"
+	value := []byte{5, 4, 3, 2, 1}
+	keyBytes := []byte(key)
+	ks, vs := len(keyBytes), len(value)
+
+	t.Logf("test write sync data =====> key: %s,  value: %v, ks: %d, vs: %d. \n", key, value, ks, vs)
+
+	record := &dataRecord{
+		dateHeader: &dataHeader{
+			Ks:   int32(ks),
+			Vs:   int32(vs),
+			Flag: 1,
+		},
+		key:   keyBytes,
+		value: value,
+	}
+	recordBuf, err := record.getRecordBuf()
+	if err != nil {
+		t.Errorf("record get buf error: %s.\n", err)
+		return
+	}
+
+	t.Logf("test write sync data =====> record bytes len: %d. \n", len(recordBuf))
+
+	c32 := crc32.ChecksumIEEE(recordBuf)
+	crcBytes, err := uintToByteSlice(c32)
+	if err != nil {
+		t.Errorf("record get crc bytes error: %s.\n", err)
+		return
+	}
+	recordBuf = append(recordBuf, crcBytes...)
+
+	key1 := "test3"
+	value1 := []byte{9, 8, 7, 6, 5}
+	keyBytes1 := []byte(key1)
+	ks1, vs1 := len(keyBytes1), len(value1)
+
+	t.Logf("test write sync data =====> key1: %s,  value1: %v, ks1: %d, vs1: %d. \n", key1, value1, ks1, vs1)
+
+	record1 := &dataRecord{
+		dateHeader: &dataHeader{
+			Ks:   int32(ks1),
+			Vs:   int32(vs1),
+			Flag: 1,
+		},
+		key:   keyBytes1,
+		value: value1,
+	}
+	recordBuf1, err := record1.getRecordBuf()
+	if err != nil {
+		t.Errorf("record get buf error: %s.\n", err)
+		return
+	}
+
+	t.Logf("test write sync data =====> record bytes len: %d. \n", len(recordBuf1))
+
+	c321 := crc32.ChecksumIEEE(recordBuf1)
+	crcBytes1, err := uintToByteSlice(c321)
+	if err != nil {
+		t.Errorf("record get crc bytes error: %s.\n", err)
+		return
+	}
+	recordBuf1 = append(recordBuf1, crcBytes1...)
+
+	recordBuf = append(recordBuf, recordBuf1...)
+
+	t.Logf("test write sync data =====> sync record bytes len: %d. \n", len(recordBuf))
+
+	if err := d.WriteSyncData(recordBuf); err != nil {
+		t.Errorf("write sync data error: %s.\n", err)
+	}
+
+}
+
+func uintToByteSlice(u uint32) ([]byte, error) {
+	s1 := make([]byte, 0)
+	buf := bytes.NewBuffer(s1)
+	err := binary.Write(buf, binary.BigEndian, u)
+	bufByte := buf.Bytes()
+	return bufByte, err
 }
